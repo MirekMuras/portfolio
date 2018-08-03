e3440c0ddc55b8b6242828793dae02db1db6ed88
Miroslav Muras <mirekmuras@gmail.com>
2018-08-03 00:20:24 -0400

    testing Express.js

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
 import config, {nodeEnv, logStars} from './config';
+import http from 'http';
 
-console.log(config, nodeEnv);
+import express from 'express';
+const server = express();
 
-logStars('function');
+server.set('view engine', 'ejs');
+
+/*try to find better server source code.
+http.createServer((req, res) => {
+  res.writeHead(500, {'Content-Type': 'text/plain'});
+  res.end('index');
+}).listen(1337, '127.0.0.1');
+*/
+
+server.get(1337,(req, res) => {
+    res.render('index');
+};
+
+
+
+console.log('Server running at http://127.0.0.1:1337/');
