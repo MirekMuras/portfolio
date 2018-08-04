599cd6bb678f7fc5cb9db917be0590445a986e07
Miroslav Muras <mirekmuras@gmail.com>
2018-08-04 18:14:19 -0400

    modified setup

--- server.js
+++ server.js
@@ -1,22 +1,34 @@
-import config, {nodeEnv, logStars} from './config';
-import http from 'http';
+
+/*import http from 'http';
 
 import express from 'express';
 const server = express();
 
-server.set('view engine', 'ejs');
+//server.set('view engine', 'ejs');
 
-/*try to find better server source code.
+//try to find better server source code.
 http.createServer((req, res) => {
   res.writeHead(500, {'Content-Type': 'text/plain'});
   res.end('index');
 }).listen(1337, '127.0.0.1');
-*/
-
-server.get('/',(req, res) => {
-    res.render('index');
-};
 
 
 
-console.log('Server running at http://127.0.0.1:1337/');
+console.log('Server running at http://127.0.0.1:1337/');*/
+
+import config from './config';
+
+import express from 'express';
+const server = express();
+
+server.get('/', (req, res) => {
+  res.send("hello");
+});
+
+server.get('/about.html', (req, res) => {
+  res.send("The about page");
+});
+
+server.listen(config.port, () => {
+  console.info('Express on port ', config.port);
+});
