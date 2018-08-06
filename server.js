bd592073c358e0a86a683dd14415d165eb2d0362
Miroslav Muras <mirekmuras@gmail.com>
2018-08-05 23:52:50 -0400

    setup

--- server.js
+++ server.js
@@ -17,17 +17,20 @@ http.createServer((req, res) => {
 console.log('Server running at http://127.0.0.1:1337/');*/
 
 import config from './config';
+import apiRouter from './api';
 
 import express from 'express';
 const server = express();
 
+server.set('view engine', 'ejs');
+
 server.get('/', (req, res) => {
-  res.send("hello");
+  res.render('index');
 });
 
-server.get('/about.html', (req, res) => {
-  res.send("The about page");
-});
+server.use('/api', apiRouter);
+server.use(express.static('public'));
+
 
 server.listen(config.port, () => {
   console.info('Express on port ', config.port);
